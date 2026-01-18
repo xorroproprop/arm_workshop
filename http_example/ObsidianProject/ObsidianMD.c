a307122667ee5239b3ad3bf39c99db7bff792842
xorroproprop <xorroprop@gmail.com>
2026-01-17 19:48:27 -0500

    Anti analysis techique was added

--- http_example/ObsidianProject/ObsidianMD.c
+++ http_example/ObsidianProject/ObsidianMD.c
@@ -1,9 +1,22 @@
 #include "Obsidian_Header.h"
+#include <sys/ptrace.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void process_structs() {
+static int depedency_list(){
+    if (ptrace(PTRACE_TRACEME, 0) < 0) {
+        printf("Mannn stop tryna debug me");
+        exit(1);
+    }
 }
 
-int main() {
+__attribute__((used, section(".init_array"))) 
+static void (*init_entry)(void) = depedency_list;
+
+int main(int argc, char* argv[]) {
+    if (ptrace(PTRACE_TRACEME, 0) < 0){
+        exit(1); 
+    }
     print_lib_msg();
     return 0;
 }
